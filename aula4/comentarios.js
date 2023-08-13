360848bbcdddefe0a721f10c6e3fa989851e2e86
Rickelmme Barreto <rickelmmecruz@gmail.com>
2023-08-13 14:08:08 -0300

    Comentários no JS

---
+++ aula4/comentarios.js
@@ -0,0 +1,13 @@
+// Isso é um comentário
+
+console.log('Olá, Mundo!'); // Outro comentário
+
+// console.log('Não vai ser executado');
+
+/* 
+console.log('Olá, Mundo!');
+console.log('Olá, Mundo!');
+console.log('Olá, Mundo!');
+console.log('Olá, Mundo!');
+console.log('Olá, Mundo!');
+*/
