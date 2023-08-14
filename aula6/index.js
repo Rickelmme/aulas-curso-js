bf13b088bd86eef2b85b039d0b90b6df3841609b
Rickelmme Barreto <rickelmmecruz@gmail.com>
2023-08-14 15:47:57 -0300

    Variáveis com let

---
+++ aula6/index.js
@@ -0,0 +1,22 @@
+// Não podemos criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Não pode começar o nome de uma variável com um número
+// Não podem conter espaços ou traços
+// Não podemos redeclarar variáveis com let
+// Utilizamos camelCase (left nomeCompleto = 'Rickelmme Barreto')
+// As variáveis são Case-sensitive
+// Não utilize var, utilize let 
+
+let idade; // Declarou a variável
+idade = '18'; // Inicializando a variável
+console.log(idade);
+idade = '15';
+console.log(idade);
+
+let nome = 'João';
+
+console.log(nome, 'nasceu em 1984.');
+console.log('Em 2000', nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2012.');
+console.log('Maria teve 1 filho com', nome, 'em 2015.');
+console.log('O filho de', nome, 'se chama Eduardo.');
