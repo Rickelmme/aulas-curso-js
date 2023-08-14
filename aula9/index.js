631d93c94aae7d4a746cceabc5f32d2feebaabf8
Rickelmme Barreto <rickelmmecruz@gmail.com>
2023-08-14 16:59:05 -0300

    Let vs Var - primeira diferença

---
+++ aula9/index.js
@@ -0,0 +1,7 @@
+// Com var pode redeclarar variáveis (Não deve ser feito isso)
+var nome = 'Rickelmme';
+var nome = 'Barreto';
+
+// Com let não pode redeclarar variáveis
+let idade = 18;
+let idade = 15;
