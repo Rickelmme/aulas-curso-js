de6120a3efa1b4961bff0981c040232a6f239f5f
Rickelmme Barreto <rickelmmecruz@gmail.com>
2023-08-14 16:16:14 -0300

    Constantes com const

---
+++ aula7/index.js
@@ -0,0 +1,20 @@
+// Não podemos criar constantes com palavras reservadas
+// Constantes precisam ter nomes significativos
+// Não pode começar o nome de uma constante com um número
+// Não podem conter espaços ou traços
+// Não pode modificar o valor de uma constante
+// Utilizamos camelCase (const nomeCompleto = 'Rickelmme Barreto')
+// As constantes são Case-sensitive
+// Não utilize var, utilize const 
+
+const nome = 'João';
+console.log(nome);
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3;
+resultadoTriplicado = resultadoTriplicado + 5;
+
+console.log(typeof primeiroNumero);
