4fb766a4fded217fb28e7f9aff239c04894a6d80
Rickelmme Barreto <rickelmmecruz@gmail.com>
2023-08-15 16:33:36 -0300

    Exercício com alert e prompt

---
+++ aula11/js/script.js
@@ -0,0 +1,9 @@
+let num1 = prompt('Digite o primeiro númuero');
+let num2 = prompt('Digite o segundo númuero');
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+const resultado = num1 + num2;
+
+alert(`O resultado de ${num1} + ${num2} é ${resultado}`);
