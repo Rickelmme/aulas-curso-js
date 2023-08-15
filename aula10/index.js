7d16fcc9d78c97a026a681b816aaa16a3bf40d14
Rickelmme Barreto <rickelmmecruz@gmail.com>
2023-08-15 15:41:17 -0300

    Operadores aritméticos, de atribuição e incremento

---
+++ aula10/index.js
@@ -0,0 +1,21 @@
+// Operadores aritméticos +  -  /  *  **  %
+// Operadores incremento  ++
+// Operadores decremento  --
+// Operadores atribuição  +=  *=  -=  /=
+
+let contador = 4;
+
+contador += 2;
+contador *= 2;
+contador -= 2;
+contador /= 2;
+
+console.log(contador);
+
+const num1 = 10;
+// const num2 = parseInt('5');
+// const num2 = parseFloat('5.2');
+const num2 = Number('5');
+
+console.log(num1 + num2);
+console.log(typeof num2);
