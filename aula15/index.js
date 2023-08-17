dc97de8285a362046e902ffd9e1815f0a7a7b6af
Rickelmme Barreto <rickelmmecruz@gmail.com>
2023-08-17 16:11:15 -0300

    Objeto Math

---
+++ aula15/index.js
@@ -0,0 +1,8 @@
+let num1 = 9.54578;
+// let num2 = Math.floor(num1); // Arredonda o número pra baixo
+// let num2 = Math.ceil(num1); // Arredonda o número pra cima
+// let num2 = Math.round(num1); // Arredonda o número para o mais próximo
+
+const aleatorio = Math.round(Math.random() * (10 - 5) + 5); // Retorna um número aleátorio
+console.log(aleatorio);
+console.log(Math.max(2,6,90,10,4)); // Retorna o maior número
