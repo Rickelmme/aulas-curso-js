b806974b4f69289d31bd802248b75773e526c960
Rickelmme Barreto <rickelmmecruz@gmail.com>
2023-08-18 15:28:01 -0300

    Arrays (básico)

---
+++ aula16/index.js
@@ -0,0 +1,13 @@
+const alunos = ['Rick', 'Maria', 'Luiz', 'Sophia', 'Thiago'];
+
+// console.log(alunos.slice(0, 3));  // Fatiamento de lista
+// delete alunos[2];  // Apaga o conteúdo do índice escolhido 
+// alunos.shift();  // Remove o primeiro ítem da lista
+// const removido = alunos.shift();  // Remove o primeiro ítem da lista e guarda o conteúdo
+// alunos.pop();  // Remove o último ítem da lista
+// const removido = alunos.pop();  // Remove o último ítem da lista e guarda o conteúdo
+// alunos.unshift('Sophia');  // Adiciona no começo da lista
+// alunos.push('Helena');  // Adiciona no final da lista
+// alunos[alunos.length] = 'Julia';  // Adiciona no final da lista
+// alunos[3] = 'Ana';  // Substitui o conteúdo do índice escolhido
+// console.log(alunos[0]);  // Mostra o conteúdo do índice escolhido
