98ca91ed5be80e014a00c9e13a95eda893db8cdb
Rickelmme Barreto <rickelmmecruz@gmail.com>
2023-08-18 14:41:11 -0300

    Exercícios com numbers e Math

---
+++ aula15/ex/js/script.js
@@ -0,0 +1,11 @@
+const numero = Number(prompt('Digite um número:'));
+const numTitulo = document.getElementById('num-titulo');
+const texto = document.getElementById('texto');
+
+numTitulo.innerHTML = numero;
+texto.innerHTML += `<p>Raiz quadrada: ${numero ** 0.5}</p>`;
+texto.innerHTML += `<p>${numero} é inteiro: ${Number.isInteger(numero)}</p>`;
+texto.innerHTML += `<p>É NaN: ${isNaN(numero)}</p>`;
+texto.innerHTML += `<p>Arredondando para baixo: ${Math.floor(numero)}</p>`;
+texto.innerHTML += `<p>Arredondando para cima: ${Math.ceil(numero)}</p>`;
+texto.innerHTML += `<p>Com duas casas decimais: ${numero.toFixed(2)}</p>`;
