8cf0dea50b70b7d5b1b010dd48151ac131f44fdb
Rickelmme Barreto <rickelmmecruz@gmail.com>
2023-08-18 16:29:02 -0300

    Funções (básico)

---
+++ aula17/index.js
@@ -0,0 +1,23 @@
+/*
+* Declaração classica de função
+function soma(x, y) {
+    const resultado = x + y;
+    return resultado;
+}
+const resultado = soma(4, 6);
+console.log(resultado);
+*/
+
+/*
+* Funcão anônima
+const raiz = function (n) {
+    return n ** 0.5;
+};
+console.log(raiz(9));
+*/
+
+/*
+* Arrow Function
+const raiz = n => return n ** 0.5;
+console.log(raiz(9));
+*/
