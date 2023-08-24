6d109dd5e8943061e0436d9c557ce51c435a8cdf
Rickelmme Barreto <rickelmmecruz@gmail.com>
2023-08-24 20:39:42 -0300

    Switch/Case

---
+++ aula29/index.js
@@ -0,0 +1,85 @@
+// ***** Utilizando Switch Case dentro de uma função *****
+function getDiaSemanaTexto (diaSemana) {
+    let diaSemanaTexto;
+    switch (diaSemana) {
+        case 0:
+            diaSemanaTexto = 'Domingo';
+            return diaSemanaTexto;
+        case 1:
+            diaSemanaTexto = 'Segunda';
+            return diaSemanaTexto;
+        case 2:
+            diaSemanaTexto = 'Terça';
+            return diaSemanaTexto;
+        case 3:
+            diaSemanaTexto = 'Quarta';
+            return diaSemanaTexto;
+        case 4:
+            diaSemanaTexto = 'Quinta';
+            return diaSemanaTexto;
+        case 5:
+            diaSemanaTexto = 'Sexta';
+            return diaSemanaTexto;
+        case 6:
+            diaSemanaTexto = 'Sábado';
+            return diaSemanaTexto;
+        default:
+            diaSemanaTexto = 'Error';
+            return diaSemanaTexto;
+    }    
+}
+
+const data = new Date('2005-01-04 00:00:00');
+const diaSemana = data.getDay();
+const diaSemanaTexto = getDiaSemanaTexto(diaSemana);
+
+console.log(diaSemana, diaSemanaTexto);
+
+// ***** Utilizando Switch Case *****
+// switch (diaSemana) {
+//     case 0:
+//         diaSemanaTexto = 'Domingo';
+//         break;
+//     case 1:
+//         diaSemanaTexto = 'Segunda';
+//         break;
+//     case 2:
+//         diaSemanaTexto = 'Terça';
+//         break;
+//     case 3:
+//         diaSemanaTexto = 'Quarta';
+//         break;
+//     case 4:
+//         diaSemanaTexto = 'Quinta';
+//         break;
+//     case 5:
+//         diaSemanaTexto = 'Sexta';
+//         break;
+//     case 6:
+//         diaSemanaTexto = 'Sábado';
+//         break;
+//     default:
+//         diaSemanaTexto = 'Error';
+// }
+// // console.log(diaSemana, diaSemanaTexto);
+
+
+// // ***** Utilizando if  else if  else *****
+// if (diaSemana === 0) {
+//     diaSemanaTexto = 'Domingo';
+// } else if (diaSemana === 1) {
+//     diaSemanaTexto = 'Segunda';
+// } else if (diaSemana === 2) {
+//     diaSemanaTexto = 'Terça';
+// } else if (diaSemana === 3) {
+//     diaSemanaTexto = 'Quarta';
+// } else if (diaSemana === 4) {
+//     diaSemanaTexto = 'Quinta';
+// } else if (diaSemana === 5) {
+//     diaSemanaTexto = 'Sexta';
+// } else if (diaSemana === 6) {
+//     diaSemanaTexto = 'Sábado';
+// } else {
+//     diaSemanaTexto = 'Error';
+// }
+// console.log(diaSemana, diaSemanaTexto);
