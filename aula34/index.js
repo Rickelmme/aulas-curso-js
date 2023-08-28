3eed978b6bc2f43e64e5af9927deff49555c0de3
Rickelmme Barreto <rickelmmecruz@gmail.com>
2023-08-28 18:10:33 -0300

    For - Clássico - Estrutura de repetição

---
+++ aula34/index.js
@@ -0,0 +1,17 @@
+// i -> index
+
+// for (let i=0; i<=10; i++) {
+//     console.log(i, par);
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//     const par = i % 2 === 0 ? 'par' : 'impar';
+//     console.log(i, par);
+// }
+
+const frutas = ['Maçã', 'Pêra', 'Uva', 'Abacaxi'];
+
+for (let i = 0; i < frutas.length; i++) {
+    // console.log(frutas[i]);
+    console.log(`Índice ${i}`, frutas[i]);
+}
