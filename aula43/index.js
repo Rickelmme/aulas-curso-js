1bb522eab462dd6465ea1b2647a9035aee4ecdd1
Rickelmme Barreto <rickelmmecruz@gmail.com>
2023-09-01 16:13:16 -0300

    Exercício com lógica de programação - 03

---
+++ aula43/index.js
@@ -0,0 +1,11 @@
+function fizzBuzz (numero) {
+    if (typeof numero !== 'number') return numero;
+    else if (numero % 5 === 0 && numero % 3 === 0) return 'FizzBuzz';
+    else if (numero % 3 === 0) return 'Fizz';
+    else if (numero % 5 === 0) return'Buzz';
+    else return numero;
+}
+
+for (let i = 0; i <= 100; i++) {
+    console.log(i, fizzBuzz(i));
+}
