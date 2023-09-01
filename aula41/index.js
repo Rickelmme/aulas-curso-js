bc24b10d414d6ec4da7639929ae581fd08f64c16
Rickelmme Barreto <rickelmmecruz@gmail.com>
2023-09-01 14:49:54 -0300

    Exercício com lógica de programação - 01

---
+++ aula41/index.js
@@ -0,0 +1,11 @@
+// Escreva uma função que recebe 2 números e retorne o maior deles
+
+// Solução 1
+function max(x, y) {
+    return x > y ? x : y;
+}
+console.log(max(20, 40));
+
+// Solução 2
+const max2 = (x, y) => x > y ? x : y;
+console.log(max2(60, 80));
