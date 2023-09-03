04818983c7236857004718d32b11b9a3807a60ae
Rickelmme Barreto <rickelmmecruz@gmail.com>
2023-09-03 14:31:50 -0300

    Tratando e lançando erros (try, catch, throw)

---
+++ aula44/index.js
@@ -0,0 +1,16 @@
+function soma(x, y) {
+    if (
+        typeof x !== 'number' || 
+        typeof y !== 'number'
+    ) {
+        throw new Error('x e y precisam ser números.');
+    }
+    return x + y;
+}
+try {
+    console.log(soma(1, 2));
+    console.log(soma('1', 2));
+} catch(error) {
+    // console.log(error); // Exibe o erro completo
+    console.log('Alguma coisa mais amigável pro usuário.');
+}
