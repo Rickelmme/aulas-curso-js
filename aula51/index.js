6b1b78825c0ad308a42b308eb8c4b0183dfcac28
Rickelmme Barreto <rickelmmecruz@gmail.com>
2023-09-09 12:50:30 -0300

    Retorno da função

---
+++ aula51/index.js
@@ -0,0 +1,12 @@
+function criaMultiplicador(multiplicador) {
+    return function(numero) {
+        return numero * multiplicador;
+    };
+}
+const duplica = criaMultiplicador(2);
+const triplica = criaMultiplicador(3);
+const quadriplica = criaMultiplicador(4);
+
+console.log(duplica(2));
+console.log(triplica(2));
+console.log(quadriplica(2));
