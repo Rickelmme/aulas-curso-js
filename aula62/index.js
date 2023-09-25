f38b020b5a82d020871c9e8997c4b4385da26b3c
Rickelmme Barreto <rickelmmecruz@gmail.com>
2023-09-25 20:25:47 -0300

    Revisão do básico em Arrays

---
+++ aula62/index.js
@@ -0,0 +1,23 @@
+/* REVISÃO ARRAY
+const nomes = ["Eduardo", "Maria", "Joana", "Luiz", "Yago"];
+const novo = nomes.slice(0, -1);
+
+const removido = nomes.pop();  Remove do final
+const removido = nomes.shift();  Remove do começo
+nomes.push("Rick");  Adiciona no final
+nomes.unshift("Bruna");  Adiciona no começo
+
+console.log(novo);
+*/
+
+/* STRING PARA ARRAY
+const nome = "Rickelmme Barreto Cruz";
+const nomeArray = nome.split(' ');
+console.log(nomeArray);
+*/
+
+/* ARRAY PARA STRING
+const nomes = ["Rickelmme", "Barreto", "Cruz"];
+const nome = nomes.join(' ');
+console.log(nome);
+*/
